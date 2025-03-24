1c39ce0af599e32344c649daca791c688e5b5347
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-03-24 15:07:59 +0300

    created a view for the users app

--- inventory_management/users/views.py
+++ inventory_management/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework import status, generics
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework.decorators import api_view
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    user = authenticate(username=username, password=password)
+    if user:
+        return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+    return Response({"error": "Invalid"}, status=status.HTTP_400_BAD_REQUEST)
