176a6a23fb5b121b88e341d28425569eee1aaba2
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-03-27 08:01:11 +0300

    created the API for the inventory app

--- inventory_management/inventory/views.py
+++ inventory_management/inventory/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Inventory
+from .serializers import InventorySerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
-# Create your views here.
+
+
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def inventory_list(request):
+    if request.method == 'GET':
+        items = Inventory.objects.all()
+        serializer = InventorySerializer(items, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = InventorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
