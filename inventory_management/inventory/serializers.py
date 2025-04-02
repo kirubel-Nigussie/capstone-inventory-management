9a7e38687f87719b25663567867b8049c9c0828b
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-04-02 19:29:47 +0300

    corrected the serializers.py inside the inventory

--- inventory_management/inventory/serializers.py
+++ inventory_management/inventory/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import Inventory
+from .models import Inventory, InventoryChangeLog
 
 class InventorySerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    date_added = serializers.ReadOnlyField()
+    last_updated = serializers.ReadOnlyField()
+
     class Meta:
         model = Inventory
         fields = '__all__'
+
+class InventoryChangeLogSerializer(serializers.ModelSerializer):
+    item_name = serializers.ReadOnlyField(source='item.name')
+    changed_by = serializers.ReadOnlyField(source='changed_by.username')
+
+    class Meta:
+        model = InventoryChangeLog
+        fields = ['id', 'item', 'item_name', 'changed_by', 'change_type', 'quantity_change', 'timestamp']
