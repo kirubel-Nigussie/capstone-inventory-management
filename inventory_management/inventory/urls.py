8975df338a6d3835d363534cce512e8e3d9dee22
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-04-04 15:20:42 +0300

    edited the urls paths inventory and the inventory_management

--- inventory_management/inventory/urls.py
+++ inventory_management/inventory/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import inventory_list
+from .views import (
+    InventoryListCreateView, InventoryRetrieveUpdateDeleteView,
+    restock_inventory, sell_inventory, InventoryChangeLogView
+)
 
 urlpatterns = [
-    path('', inventory_list, name='inventory-list'),
+    path('inventory/', InventoryListCreateView.as_view(), name='inventory-list-create'),
+    path('inventory/<int:pk>/', InventoryRetrieveUpdateDeleteView.as_view(), name='inventory-detail'),
+    path('inventory/<int:pk>/restock/', restock_inventory, name='restock-inventory'),
+    path('inventory/<int:pk>/sell/', sell_inventory, name='sell-inventory'),
+    path('inventory/logs/', InventoryChangeLogView.as_view(), name='inventory-change-logs'),
 ]
--- inventory_management/inventory_management/urls.py
+++ inventory_management/inventory_management/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('users.urls')),
-    path('inventory/', include('inventory.urls')),
+    path('api/users/', include('users.urls')),
+    path('api/', include('inventory.urls')),
 
 ]
