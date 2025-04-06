a319a4989ca5677186fb5acdd5fe6ed354fe97f3
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-04-06 14:24:33 +0300

    add rest_framework.authtoken in the sttings.py and edited the users views.py file to generet generate token when login

--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
     "inventory.apps.InventoryConfig",
     "rest_framework",
+    "rest_framework.authtoken",
     
 ]
 
@@ -125,3 +126,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
--- inventory_management/users/views.py
+++ inventory_management/users/views.py
@@ -1,20 +1,25 @@
-from django.shortcuts import render
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status, generics
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.authtoken.models import Token  
+from rest_framework.permissions import AllowAny
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login_user(request):
     username = request.data.get("username")
     password = request.data.get("password")
     user = authenticate(username=username, password=password)
     if user:
-        return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
-    return Response({"error": "Invalid"}, status=status.HTTP_400_BAD_REQUEST)
+        token, created = Token.objects.get_or_create(user=user) 
+        return Response({"message": "Login successful!",
+                         "token": token.key})
+    return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
